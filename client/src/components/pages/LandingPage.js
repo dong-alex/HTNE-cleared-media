3c08de0ae3ac2ff3001b7bd91ee63bd062b227eb
Kynan <kynan.ly@hotmail.ca>
2020-06-07 05:05:28 -0600

    Added an image

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -5,7 +5,8 @@ const LandingPage = (props) => {
 	return (
 		<Layout>
 			<div>
-				<h1>Find out how we talk</h1>
+				<h1>How we see media</h1>
+				<img src="https://clientinsight.ca/wp-content/uploads/2017/04/CI-Blog-170404-B22-clarity-std-sz-2.jpg"></img>
 			</div>
 		</Layout>
 	);
