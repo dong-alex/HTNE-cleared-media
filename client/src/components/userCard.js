bdf4b99d638399114ad3ba86ff125d0abb4f44cd
dong-alex <adong@ualberta.ca>
2020-06-07 01:48:37 -0600

    remove unused import

--- client/src/components/userCard.js
+++ client/src/components/userCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Image from "material-ui-image";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 
